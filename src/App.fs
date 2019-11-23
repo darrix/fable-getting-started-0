6af9e53cb226a40e60e328d78faae8b55e6cece8
darrix <dmhendricks@gmail.com>
2019-11-23 10:38:29 -0500

    Updates after class one

--- src/App.fs
+++ src/App.fs
@@ -1,3 +1,27 @@
 module App
 
+open Browser.Dom
+
+
+let plus = document.getElementById "plus"
+let minus = document.getElementById "minus"
+let textcount = document.getElementById "textcount"
+
+
+
+let mutable count = 0
+
+let updateTextCount () =
+    textcount.innerHTML <- sprintf "%d" count 
+
+updateTextCount ()
+
+plus.addEventListener("click", fun _ -> count <- count + 1; updateTextCount())
+minus.addEventListener("click", fun _ -> count <- count - 1; updateTextCount())
+
+let a = 14
+let b = 30
+let c = a + b
+printfn "here's a number %i" c
+
 printfn "Hello from Fable"
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 var path = require("path");
 
+// check environment
+var isProduction = !process.argv.find(v => v.indexOf('webpack-dev-server') !== -1);
+console.log('Bundling for ' + (isProduction ? 'production' : 'development') + '...');
+
 module.exports = {
-    mode: "none",
+    mode: "development",
     entry: "./src/App.fsproj",
+    output: {
+        path: path.join(__dirname, "./public"),
+        filename: "main.js"
+    },
     devServer: {
         contentBase: path.join(__dirname, "./dist")
     },
+    devtool: isProduction ? 'source-map' : 'eval-source-map',
     module: {
         rules: [{
             test: /\.fs(x|proj)?$/,
